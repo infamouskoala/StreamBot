d85428438d907d19490558c4aa8b77bec8127c50
Infamous Koala <75808182+infamous-koala@users.noreply.github.com>
2023-04-02 13:20:23 +0530

    Add files via upload

---
+++ streamBot/run.py
@@ -0,0 +1,19 @@
+import discord
+import os
+from discord.ext import commands
+from keep_alive import keep_alive
+import time
+
+client=commands.Bot(command_prefix=':', self_bot=True, help_command=None)
+
+TOKEN = input("enter token: ")
+status = input("Status message: ")
+
+@client.event
+async def on_ready():
+    os.system('clear')
+    await client.change_presence(activity=discord.Streaming(name=status, url="https://twitch.tv/infamouskoala"))
+    print(f'Logged in as {client.user} ({client.user.id})')
+
+keep_alive()
+client.run(TOKEN, bot=False)
