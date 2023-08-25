6649d1d4aa0d6309e0c4a90bc0b6a3ffc36822c8
Infamous Koala <75808182+infamous-koala@users.noreply.github.com>
2023-08-25 19:03:14 +0530

    Update run.py

--- streamBot/run.py
+++ streamBot/run.py
@@ -12,7 +12,7 @@ status = input("Status message: ")
 @client.event
 async def on_ready():
     os.system('clear')
-    await client.change_presence(activity=discord.Streaming(name=status, url="https://twitch.tv/infamouskoala"))
+    await client.change_presence(activity=discord.Streaming(name=status, url="https://twitch.tv/koala_from_mars"))
     print(f'Logged in as {client.user} ({client.user.id})')
 
 keep_alive()
